e6f577ab187c1ad6484998d25748bcd4fcea29cb
Lee Zheng Han <e0544595@u.nus.edu>
2021-05-14 12:04:57 +0800

    Fix git not updating lower-case directories

--- src/main/java/events/OpenCrate.java
+++ src/main/java/events/OpenCrate.java
@@ -15,7 +15,6 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
 import commands.SetCrate;
 import plugin.CustomShop;
 
