edb49d458e4a995c576471df3e096765b19587d8
Lee Zheng Han <e0544595@u.nus.edu>
2021-05-17 13:53:14 +0800

    Items drop when shop is removed

--- src/main/java/customshop/shop/vm/VMRemover.java
+++ src/main/java/customshop/shop/vm/VMRemover.java
@@ -3,28 +3,44 @@ package customshop.shop.vm;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
+import org.bukkit.block.ShulkerBox;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
+import customshop.plugin.CustomShop;
 import customshop.shop.ShopRemover;
 
 /**
  * Vending machine's shop remover.
  */
 public class VMRemover extends ShopRemover {
-    Entity shopEntity;
+    ArmorStand armorStand;
 
-    public VMRemover(Block targetBlock, Entity shopEntity) {
+    public VMRemover(Block targetBlock, ArmorStand armorStand) {
         super(targetBlock);
-        this.shopEntity = shopEntity;
+        this.armorStand = armorStand;
     }
 
     @Override
     public void removeShop() {
-        Location bottom = shopEntity.getLocation();
-        Location top = shopEntity.getLocation();
+        Location bottom = armorStand.getLocation();
+        Location top = armorStand.getLocation();
         top.setY(top.getY() + 1);
         bottom.getBlock().setType(Material.AIR);
         top.getBlock().setType(Material.AIR);
-
-        shopEntity.remove();
+        ItemStack chestItem = armorStand.getEquipment().getChestplate();
+        if (chestItem == null || !(chestItem.getItemMeta() instanceof BlockStateMeta)
+                || !(((BlockStateMeta) chestItem.getItemMeta()).getBlockState() instanceof ShulkerBox)) {
+            CustomShop.getPlugin().getServer().getConsoleSender()
+                    .sendMessage("§c§l[CustomShop] Attempting to remove vending machine at " + bottom
+                            + " with missing shulker box!");
+        } else {
+            ShulkerBox shulker = (ShulkerBox) ((BlockStateMeta) chestItem.getItemMeta()).getBlockState();
+            shulker.getInventory().forEach(item -> {
+                if (item != null)
+                    armorStand.getWorld().dropItemNaturally(armorStand.getLocation(), item);
+            });
+        }
+        armorStand.remove();
     }
 }
