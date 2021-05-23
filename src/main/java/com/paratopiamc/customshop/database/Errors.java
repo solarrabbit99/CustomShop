4d3a4d90e5634bb42d905fc067ae635d52a7f670
Lee Zheng Han <e0544595@u.nus.edu>
2021-05-23 17:54:01 +0800

    Add gpl header to database files

--- src/main/java/com/paratopiamc/customshop/database/Errors.java
+++ src/main/java/com/paratopiamc/customshop/database/Errors.java
@@ -1,7 +1,24 @@
+/*
+ *  This file is part of CustomShop. Copyright (c) 2021 Paratopia.
+ *
+ *  CustomShop is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CustomShop is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CustomShop. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.paratopiamc.customshop.database;
 
 import java.util.logging.Level;
-
 import com.paratopiamc.customshop.plugin.CustomShop;
 
 /** Error loggers. */
